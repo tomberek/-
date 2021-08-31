db11091efc268c392c799673d808347c6633865b
Tom Bereknyei <tomberek@gmail.com>
2021-08-30 20:28:12 -0400

    init pkgsMerge

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Chained packages";
+  inputs.nixpkgs.url = "nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" ]; #"x86_64-darwin" "aarch64-linux"];
+
+      # Helper functions
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system;
+          overlays = [ self.overlay ]; });
+    in {
+      overlay = self: super: {
+        "-" = self.pkgsMerge;
+        "+" = self.pkgsMerge;
+        pkgsMerge =
+          let
+            gen = name: paths: self.buildEnv {
+              inherit name paths;
+              ignoreCollisions = true;
+              meta.mainProgram = let
+                last = self.lib.last paths; in last.meta.mainProgram
+                or (builtins.parseDrvName last.name).name;
+
+              # Use lists not attrsets because order matters
+              passthru = with builtins; mapAttrs (n: v: gen
+                  (if length paths > 5 then "merged-environment" else "${name}-${n}")
+                  (paths ++ [ v ])
+                ) super;
+            };
+          in gen "merged" [ ];
+      };
+
+      legacyPackages = forAllSystems (system: nixpkgsFor.${system});
+      packages = forAllSystems (system: { inherit (nixpkgsFor.${system})
+        pkgsMerge "+" "-"; });
+
+      defaultPackage = forAllSystems (system: self.packages."${system}".pkgsMerge);
+    };
+}
